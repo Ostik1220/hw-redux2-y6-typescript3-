e1c49fcfe18b651265ad1e5c73fc6bbaae20bc03
Ostik <117195748+Ostik1220@users.noreply.github.com>
2026-07-19 00:21:19 +0300

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,63 @@
 import "./App.css";
 
 function App() {
-  // 1) створіть абстрактний клас House, в ньому повинна бути наступна логіка:
 
-  // властивість door – вона може бути закрита, або відкрита.
-  // властивість key – об'єкт класу Key.
-  // конструктор приймає аргумент класу Key та зберігає його у властивість key.
-  // метод comeIn, який додає об'єкт класу Person у властивість tenants і це спрацьовує лише, якщо door відкрита.
-  // абстрактний метод openDoor приймає аргумент класу Key
-  // 	2) Створіть клас MyHouse, який реалізує клас House
-    // створюємо метод openDoor, оскільки він приймає ключ, звіряємо збережений ключ у властивості key чи дорівнює він ключу з аргументу, якщо так, відкриваємо двері.
-  // 	3) Створіть об'єкт Key
-  // є тільки властивість signature
-  // під час створення об'єкта генерує випадкове число та зберігає його у signature
-  // метод getSignature повертає випадкове число з signature
-  // 	4) Створіть об'єкт Person
-  // конструктор приймає ключ класу Key і зберігає його у властивість key
-  // метод getKey повертає key
+  function getPromise ()  : Promise<[string, number]> { {
+	 return new Promise((resolve)=> {
+	   resolve(['Text', 50]);
+	 });
+	}}
 
-  // 	5) Зробіть так, щоб мешканець потрапив додому.
+	getPromise ()
+	.then((data) => {
+	 console.log(data);
+	});
 
+  	type AllType = {
+	 name: string;
+	 position: number;
+	 color: string;
+	 weight: number
+	}
 
-  abstract class House {
-    constructor(
-      public door: boolean,
-      protected key: Key,
-      public tenants: Person[]
-    ){}
+  	function compare (top: Pick<AllType, 'name' | 'color'>, bottom: Pick<AllType, 'position' | 'weight'>): AllType {
+	 return {
+	   name: top.name,
+	   color: top.color,
+	   position: bottom.position,
+	   weight: bottom.weight,
+	 }
+	}
 
-    comeIn(Person: Person) {
-      if (this.door) {
-        console.log("Welcome home");
-        this.tenants.push(Person);
-      } else {
-        console.log("Door is closed");
-      }
-    }
-
-    openDoor(key: Key) {}
-  }
-
-  class MyHouse extends House {
-    openDoor(key: Key) {
-      // console.log(key.getSignature());
-      // console.log(this.key.getSignature());
-      if (this.key.signature === key.signature) {
-        console.log("Door is open");
-        this.door = true;
-      } else {
-        this.door = false;
-        console.log("Wrong key");
-      }
-    }}
-
-  class Key {
-    signature: number;
-
-    constructor() {
-      this.signature = Math.floor(Math.random() * 1000);
-    }
-
-    getSignature(): number {
-      return this.signature;
-    }
-  }
-
-  class Person {
-    constructor(readonly key: Key, public name: string) {} 
-
-    getKey() : Key {
-      return this.key;
-    }
-  }
-
-  const Ostap = new Person(new Key(), "Ostap");
-  const Maksym = new Person(new Key(), "Maksym");
-  const Home = new MyHouse(false, Ostap.getKey(), []);
-
-  console.log(Ostap.getKey().getSignature());
-  console.log(Maksym.getKey().getSignature());
-
-  console.log(Ostap.getKey().getSignature());
-  Home.openDoor(Ostap.getKey());
-  Home.comeIn(Ostap);
-  console.log(Home.tenants);
-  Home.openDoor(Maksym.getKey());
-  Home.comeIn(Maksym);
-  console.log(Home.tenants);
+  console.log(compare({name: 'Name', color: 'red'}, {position: 1, weight: 100}));
 
 
+  // function merge<T extends object, U extends object>(objA: T, objB: U) { я потім побачив таке вирішення в конспекті але не знаю чи моє теж підходить
+function merge<U>(objA:Object , objB: U) { 
+  return Object.assign(objA, objB)
+}
+
+  const wert = merge({name: "Rook"}, {id:2})
+
+  console.log(wert.id)
+
+  
+
+
+
+  class Component <T> {
+	 constructor (public props:T) {
+
+	 }
+	}
+
+
+
+	class Page extends Component{
+	 pageInfo () {
+	   console.log(this.props.title);
+	 }
+	}
 
 
   return <></>;
